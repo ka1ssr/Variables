b0b03b7d9fc72f45c13c31dc6fc29b354caa43c5
ka1ssr <ka1ssr@ernie>
2017-05-28 18:56:42 -0400

    First working commit.

---
+++ src/variables/Variables.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author ka1ssr
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int a = 2, b = 2;
+        int sum = a + b;
+        
+        System.out.println(a+"+"+b+"="+sum);
+    }
+    
+}
